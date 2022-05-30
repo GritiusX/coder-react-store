8217189d7467d3ec59dfc3b47e94c35a8944e995
Guido Pawluk <guidopawluk@gmail.com>
2022-05-29 21:11:40 -0300

    Agregamos todo lo pedido por el desafio

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CartWidget.css';
+import { GiShoppingCart } from "react-icons/gi";
+
+
+function CartWidget() {
+  return (
+    <GiShoppingCart className="red" />
+  )
+}
+
+export default CartWidget
