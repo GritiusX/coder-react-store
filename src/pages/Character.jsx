61f3dc347868fd8d6a4bcceef0fd0cffb7789541
Guido Pawluk <guidopawluk@gmail.com>
2022-06-13 00:23:05 -0300

    mañana terminar el itemDetail y el itemContainer y gg

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -4,11 +4,12 @@ import { Link, useParams } from "react-router-dom";
 export default function Character() {
   const {id} = useParams();
   useEffect(() => {
+    console.log(id);
   }, [id]);
   
   return (
     <>
-     
+     <h1>{id}</h1>
       <Link to="/">ir a home</Link>
     </>
   );
