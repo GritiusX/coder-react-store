9f1754e2981fb97ef94db21dc673782fcf51de26
GritiusX <guidopawluk@gmail.com>
2022-06-27 23:26:01 -0300

    acomodamos el cart

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,38 +4,37 @@ import { MyContext } from "../context/CartContext.jsx";
 import "./ItemDetail.css";
 
 export default function ItemDetail({ product }) {
-  const { isInCart, addItem } = useContext(MyContext);
-  const [qty, setQty] = useState(1);
+	const { isInCart, addItem } = useContext(MyContext);
+	const [qty, setQty] = useState(1);
 
-  const onAdd = () => {
-    alert(`se agregaron ${qty} items`);
-    isInCart(product.id);
-    addItem(product, qty);
-  };
+	const onAdd = () => {
+		isInCart(product.id);
+		addItem(product, qty);
+	};
 
-  return (
-    <>
-      <section className="container d-flex flex-column justify-content-center align-items-center mt-5">
-        <article className="row justify-content-md-center">
-          <div className="col justify-content-md-center">
-            <img src={product?.thumbnail} alt={product?.title} />
-          </div>
-          <div className="col justify-content-md-center">
-            <h1 className="row">Nombre: {product?.title}</h1>
-            <p className="row">Description: {product?.description}</p>
-            <p className="row">Price: ${product?.price}</p>
-            <p className="pe">Rating:</p>
-            <p className="pe">{product?.rating}</p>
-            <p className="pe">stock: {product?.stock}</p>
-          </div>
-        </article>
-        <ItemCount
-          qty={qty}
-          setQty={setQty}
-          stock={product.stock}
-          onAdd={onAdd}
-        />
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="container d-flex flex-column justify-content-center align-items-center mt-5">
+				<article className="row justify-content-md-center">
+					<div className="col justify-content-md-center">
+						<img src={product?.thumbnail} alt={product?.title} />
+					</div>
+					<div className="col justify-content-md-center">
+						<h1 className="row">Nombre: {product?.title}</h1>
+						<p className="row">Description: {product?.description}</p>
+						<p className="row">Price: ${product?.price}</p>
+						<p className="pe">Rating:</p>
+						<p className="pe">{product?.rating}</p>
+						<p className="pe">stock: {product?.stock}</p>
+					</div>
+				</article>
+				<ItemCount
+					qty={qty}
+					setQty={setQty}
+					stock={product.stock}
+					onAdd={onAdd}
+				/>
+			</section>
+		</>
+	);
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,21 +7,52 @@ export default function Cart() {
 	console.log(carrito);
 	return (
 		<div>
-			<div>{JSON.stringify(carrito)}</div>
+			{/* <div>{JSON.stringify(carrito)}</div> */}
 			{carrito.length > 0
 				? carrito.map((prods) => (
-						<article key={prods.id}>
-							<div>
-								<p>{prods.qty}</p>
-								<p>{prods.title}</p>
-								<p>{prods.price}</p>
-								<p>{prods.qty * prods.price}</p>
-								<button onClick={() => deleteItem(prods.id)}>delete</button>
+						<article className="mt-3 bg-secondary p-3 mx-3" key={prods.id}>
+							<div className="container-fluid">
+								<div className="row">
+									<div className="col">
+										<div>
+											<img
+												style={{ width: "200px" }}
+												src={prods.thumbnail}
+												alt={prods.title}
+											/>
+										</div>
+									</div>
+									<div className="col d-flex flex-column justify-content-center">
+										<p className="fw-bold">Cantidad</p>
+										<p>{prods.qty}</p>
+									</div>
+									<div className="col d-flex flex-column justify-content-center">
+										<p className="fw-bold">Producto</p>
+										<p>{prods.title}</p>
+									</div>
+									<div className="col d-flex flex-column justify-content-center">
+										<p className="fw-bold">Precio unitario</p>
+										<p>{prods.price}</p>
+									</div>
+									<div className="col d-flex flex-column justify-content-center">
+										<p className="fw-bold">Precio total</p>
+										<p>{prods.qty * prods.price}</p>
+									</div>
+									<div className="col d-flex flex-column justify-content-center">
+										<button
+											type="button"
+											className="btn btn-danger"
+											onClick={() => deleteItem(prods.id)}
+										>
+											delete
+										</button>
+									</div>
+								</div>
 							</div>
 						</article>
 				  ))
 				: "empty"}
-			{getItemPrice()}
+			<p className="fw-bold">Total carrito: ${getItemPrice()}</p>
 			<br />
 			<Link to="/">Volver a Home </Link>
 		</div>
